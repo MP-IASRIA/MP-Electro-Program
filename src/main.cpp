7adf30b54451cb2fc0dbf287fdc668526ec2ed62
R. ALOUI <aloui.radhouane@gmail.com>
2023-01-13 02:53:53 +0100

    ECG on monitoring screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 #include <Arduino.h>
 
-void setup() {
-  Serial.begin(115200);
-  Serial.print("Programme d analyse des signaux ECG");
+#define LED_BUILTIN 2 // pin with LED to turn on when BT connected
+
+void setup()
+{
+  // initialize digital pin 2 as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize the serial communication:
+  Serial.begin(9600);
+  Serial.println();   // blank line in serial ...
+  pinMode(41, INPUT); // Setup for leads off detection LO +
+  pinMode(40, INPUT); // Setup for leads off detection LO -
 }
 
-void loop() {
-  
+void loop()
+{
+  if ((digitalRead(40) == 1) || (digitalRead(41) == 1))
+  {
+    Serial.println('!');
+  }
+  else
+  {
+    // send the value of analog input 0 to serial:
+    Serial.println(analogRead(A0));
+  }
+  // Wait a little to keep serial data from saturating
+  delay(500);
 }
